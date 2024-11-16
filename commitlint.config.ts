27ae2314cc91394c58a449ab1d4a6e85adedbd49
Muhammad Aziz Prasetyo <azizprasetyo100901@gmail.com>
2024-11-16 11:47:46 +0700

    conf(eslint): add lint-staged and commitlint
    - add lint-staged config to run prettier and eslint on staged files
    - add lint-staged to pre-commit hook
    - add lint-staged to pre-push hook
    - enable commitlint with husky

---
+++ commitlint.config.ts
@@ -0,0 +1,32 @@
+import type { UserConfig } from "@commitlint/types";
+
+const commitlintConfig: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build", // Changes to the build system or dependencies
+        "chore", // Routine tasks not affecting the application code
+        "ci", // Changes to Continuous Integration configuration
+        "cd", // Changes to Continuous Deployment setup
+        "docs", // Documentation updates
+        "feat", // Addition of a new feature
+        "fix", // Bug fixes
+        "perf", // Performance improvements
+        "improvement", // Enhancements to an existing feature
+        "refactor", // Code restructuring without functionality changes
+        "revert", // Reverting a previous commit
+        "style", // Code formatting or style changes
+        "test", // Adding or updating tests
+        "conf", // Adjustments to configuration files
+        "lib", // Modifications to internal libraries or modules
+        "prerelease", // Preparation for pre-release versions
+        "release", // Release of a new version
+      ],
+    ],
+  },
+};
+
+export default commitlintConfig;
